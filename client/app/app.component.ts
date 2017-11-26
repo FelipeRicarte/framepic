f8006291775c28e5a9e4e48cd18c839d51ae0a98
Felipe Ricarte <felipericartem@gmail.com>
2017-11-26 13:29:16 -0300

    Criação do componente de fotos

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
+
+    moduleId:module.id,
     selector: 'app',
     templateUrl: './app/app.component.html'
 })
---
+++ client/foto/foto.module.ts
@@ -0,0 +1,9 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FotoComponent}   from './foto.component';
+
+@NgModule({
+  declarations: [ FotoComponent ],
+  exports: [FotoComponent]
+})
+export class FotoModule{ }
